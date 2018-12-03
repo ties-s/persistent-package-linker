72fbfee0463b95b334a3398008c9e69339ac9f17
Ties <ties.schwertasek@gmail.com>
2018-12-03 05:14:44 +0100

    Remove dist files from git

--- dist/actions.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const postInstall: () => void;
-export declare const linkPackage: (packages: string[]) => Promise<void>;
-export declare const unlinkPackage: (packages: string[]) => Promise<void>;
-export declare const linkAll: () => Promise<void>;
-export declare const linkSelf: () => void;
-export declare const setupLinking: () => Promise<void>;
-export declare const setupHook: () => Promise<void>;
